b31eab6ed74299193479c632aa7e26cb009dfb83
mateo9 <36419835+mateo9@users.noreply.github.com>
2018-02-12 23:25:42 +0100

    Add files via upload

---
+++ Duplicate Encoder.js
@@ -0,0 +1,32 @@
+/* 
+  The goal of this exercise is to convert a string to a new string where each
+  character in the new string is '(' if that character appears only once in the original string, or ')' if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples:
+
+"din" => "((("
+
+"recede" => "()()()"
+
+"Success" => ")())())"
+
+"(( @" => "))((" 
+*/
+
+
+function duplicateEncode(word){
+    var str = "";
+     w = word.toLowerCase();
+    for(let i = 0; i < w.length; i ++)
+    {
+      if(w.indexOf(w[i]) === w.lastIndexOf(w[i]))
+      {
+        str += '(';
+      } else str += ')';
+    
+    }
+
+    return str;
+}
+
+
