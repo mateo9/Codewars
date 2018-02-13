eaba8cd0c4d15c835a564f965e29ea3076dd941e
mateo9 <36419835+mateo9@users.noreply.github.com>
2018-02-13 14:11:55 +0100

    Update Stop gninnipS My sdroW!.js

--- Stop gninnipS My sdroW!.js
+++ Stop gninnipS My sdroW!.js
@@ -1,39 +1,32 @@
 /* 
 
-Program channels into your TV's memory. An array with channels (strings) will be passed as an
-argument to the function redarr(). Sort the channels in an alphabetical order, remove duplicates
-and, finally, return an object where each channel (object's value) is assigned to a whole number
-(objects's key), starting with 0.
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more
+letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces.
+Spaces will be included only when more than one word is present.
 
 Examples:
 
-var arr = ["BBC1", "BBC2", "MTV"];
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+spinWords( "This is a test") => returns "This is a test"
+spinWords( "This is another test" )=> returns "This is rehtona test"
 
-redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
-
-var arr = ["BBC1", "BBC1", "BBC2", "MTV"];
-
-redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
 
 
 */
 
-
-function redarr(arr) {
-
-var a = [];
-var obj = {};
- a = arr.filter((current,index,array) => {
-	return arr.indexOf(current) == index;
-});
-a.sort();
-obj = a.reduce((acc,current,index,array) => {
-    acc[index] = current;
-    return acc;
-},obj);
-
-
-return obj;
-
+function spinWords(str){
+  var arr = str.split(" ");
+  
+  var test = arr.map(function(current) {
+  	if(current.length < 5) {
+  		return current;
+  	} else {
+  		return current.split("").reverse().join("");
+  	}
+  });
+  test = test.join(" ");
+  return test;
 }
+
+spinWords("Hey fellow warriors");
 
